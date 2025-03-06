fbf936cf8366d23fa6fb7311b696248964a8b8c7
sannimdev <crontables@gmail.com>
2025-03-06 14:32:52 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/Car.js
@@ -0,0 +1,11 @@
+class Car {
+  position = 0;
+
+  move() {
+    this.position += 1;
+
+    console.log("moved");
+  }
+}
+
+export default Car;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import Car from "./Car.js";
+
+const car = new Car();
+
+console.log(car.move());
